8dbd834d9b6ab4c2b5d4979971cc291b6b620a5f
Alexandr-Petrenko <alexandr.petrenko.fe@gmail.com>
2021-11-02 15:07:44 +0200

    router redirect and Error404 page were added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,25 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import Header from './shared/Header';
 import '../styles/_index.scss';
-// import Error404 from '../Error404.jsx';
 import { LoginProvider } from './Context/LoginContext.jsx';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Error404 from './Error404.jsx';
 
 const App = () => (
   <LoginProvider>
     <Router>
       <Header />
       <Switch>
-        <Route path="/login" component={Login} />
+        <Redirect exact from="/" to="/home" />
         <Route path="/home" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route component={Error404} />
       </Switch>
     </Router>
   </LoginProvider>
